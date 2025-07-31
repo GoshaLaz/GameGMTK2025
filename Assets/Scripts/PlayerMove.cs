b882ba733bcdd0d3b6af7654163733f8c56bdffb
GoshaLaz <109652165+GoshaLaz@users.noreply.github.com>
2025-07-31 12:37:53 +0300

    player movement commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove: MonoBehaviour
+{
+    public float moveTime = 0.2f;
+    public LayerMask collisionLayer; 
+    private bool isMoving = false;
+    private Vector2 input;
+    public float step;
+
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (Mathf.Abs(input.x) > 0)
+                input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                Vector2 targetPos = (Vector2)transform.position + (input*step);
+
+                if (!Physics2D.OverlapCircle(targetPos, 0.1f, collisionLayer))
+                {
+                    StartCoroutine(Move(targetPos));
+                }
+            }
+        }
+    }
+
+    IEnumerator Move(Vector2 target)
+    {
+        isMoving = true;
+
+        Vector2 start = transform.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < moveTime)
+        {
+            transform.position = Vector2.Lerp(start, target, elapsedTime / moveTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+        isMoving = false;
+    }
+}
